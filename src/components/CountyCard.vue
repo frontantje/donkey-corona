2fe3f24455d4b9fd4abee7e5080b9a23b89768a5
Antje Lemberg <frontantje@gmail.com>
2021-01-20 23:26:46 +0100

    Add basic styling

--- src/components/CountyCard.vue
+++ src/components/CountyCard.vue
@@ -22,18 +22,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.county-card {
+  max-width: 400px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
