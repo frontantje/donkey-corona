018e5b08488f3e367cf5dfacb972aee1a417afa7
Antje Lemberg <a.h.m.lemberg@gmail.com>
2021-09-30 21:37:52 +0200

    Add basic search bar component

---
+++ src/components/CountySearchbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <input v-model="searchInput" id="county-search-input" type="text" />
+</template>
+<script>
+export default {
+  name: "CountySearchbar",
+  data() {
+    return {
+      searchInput: ""
+    };
+  }
+};
+</script>
+<style>
+#county-search-input {
+  width: 100%;
+  border-radius: 15px;
+  height: 40px;
+  font-size: 1.8rem;
+}
+</style>
