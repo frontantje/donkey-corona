97a8817d3fb7b1ff621cb8ffefb638d4e5156afe
Antje Lemberg <a.lemberg@schmidtgruppe.de>
2022-01-10 12:44:43 +0100

    Add config data

---
+++ src/gravity_config.js
@@ -0,0 +1,54 @@
+const gravityConfig = [
+  {
+    cases_max: 5,
+    backgroundColor: "rgb(255, 252, 205)",
+    color: "rgb(0, 0, 0)"
+  },
+  {
+    cases_max: 5,
+    backgroundColor: "rgb(255, 243, 128)",
+    color: "rgb(0, 0, 0)"
+  },
+  {
+    cases_max: 25,
+    backgroundColor: "rgb(255, 181, 52)",
+    color: "rgb(0, 0, 0)"
+  },
+  {
+    cases_max: 100,
+    backgroundColor: "rgb(212, 54, 36)",
+    color: "rgb(255, 255, 255)"
+  },
+  {
+    cases_max: 250,
+    backgroundColor: "rgb(149, 18, 20)",
+    color: "rgb(255, 255, 255)"
+  },
+  {
+    cases_max: 500,
+    backgroundColor: "rgb(103, 18, 18)",
+    color: "rgb(255, 255, 255)"
+  },
+  {
+    cases_max: 750,
+    backgroundColor: "rgb(221, 0, 133)",
+    color: "rgb(255, 255, 255)"
+  },
+  {
+    cases_max: 1000,
+    backgroundColor: "rgb(90, 12, 145)",
+    color: "rgb(255, 255, 255)"
+  },
+  {
+    cases_max: 1500,
+    backgroundColor: "rgb(10, 6, 82)",
+    color: "rgb(255, 255, 255)"
+  },
+  {
+    cases_max: 2000000,
+    backgroundColor: "rgb(0, 0, 0)",
+    color: "rgb(255, 255, 255)"
+  }
+];
+
+export default gravityConfig;
